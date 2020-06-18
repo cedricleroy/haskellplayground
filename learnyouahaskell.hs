72d28ebc75e6581ee17ed30be7e0c93aef845a89
Cedric Leroy <cedie73@hotmail.fr>
2020-06-17 21:52:47 -0500

    Up to Texas range

--- learnyouahaskell.hs
+++ learnyouahaskell.hs
@@ -3,21 +3,48 @@
 main :: IO ()
 main = return () 
 
+-- Ready, set, go!
+successor x = succ x -- succ 9 >> 9
+mini x y = min x y -- mini 9 10 >> 9
+maxi x y = max x y -- maxi 9 10 >> 10
+
+-- Baby's first functions
 doubleMe x = x + x
 addMe x y = x + y
 doubleSmallNumber x = if x > 100 then x else x * 2
+
+-- An intro to lists
 helloWorld = "hello" ++ " " ++ "world"
 myList = [1,2,3,4] ++ [9,10,11,12]
 thirdElement = myList !! 2
+firtElement = head myList
+withoutHead = tail myList
+lastElement = last myList
+withoutLastElement = init myList
+myListLen = length myList
+myListReversed = reverse myList
+myListSliced x = take x myList -- myListSliced 3 >> [1,2,3]
+myListDroped x = drop x myList -- myListDroped 3 >> [4,9,10,11,12]
+myListMini = minimum myList
+myListMaxi = maximum myList
+sumMyList = sum myList
+productMyList = product myList
+xInMyList x = x `elem` myList -- similar to `in` in Python
+-- Texas ranges
+generate1to10 = [1..10]
+threeTo20byStepsOf3 = [3,6..20]
 listComp = [x*2 | x <- [1..10], x*2 > 12]
-
+infiniteCycle = take 10 (cycle [1,2,3]) -- slice to not have infinite print
+replicate10times3 = replicate 10 3
+-- I'm a list comprehension
 listComp' :: [Integer]
 listComp' = [x*2 | x <- [1..10], x*2 > 12]
+
+-- To sort
 doubleSmallNumber' :: Integer -> Integer
 doubleSmallNumber' x = if x > 100 then x else x * 2
 stuff x y = x * y + 1
 zipWithTest = zipWith (stuff) [1,2,3] [4,5,6]
-
 sumStrings = show . sum . map read . words
 sumStrings' x = sum [read y | y <- words x]
 
